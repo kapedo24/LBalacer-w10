db9799a17aa28d6766d65ddca32bfc0fb03ae9b5
kapedo24 <kokouapedo370@gmail.com>
2024-08-06 20:45:54 -0500

    new lb

--- backend.tf
+++ backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
-    bucket         = "chbvchcvbcvccvcv" #replace with your bucket
+    bucket         = "w7-ka-terraform" #replace with your bucket
     key            = "week10/terraform.tfstate"
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "hgdsadghgd" #replace with the table
+    dynamodb_table = "locktable" #replace with the table
   }
 }
--- setup.sh
+++ setup.sh
@@ -4,5 +4,5 @@ sudo yum update -y
 sudo yum install httpd -y
 sudo systemctl start httpd
 sudo systemctl enable httpd
-sudo bash -c 'echo "<html><body style=\"background-color: 47D34F;\"><h1><p> Welcome to Utrains the place to learn DevOps, Cloud, Linux!!.<br> This traf
+sudo bash -c 'echo "<html><body style=\"background-color: #FF00F;\"><h1><p> Welcome to Utrains the place to learn DevOps, Cloud, Linux!!.<br> This traf
 fic is served from: <span style=\"color: purple;\">${HOSTNAME}</span> </p></h1></body></html>" > /var/www/html/index.html'
